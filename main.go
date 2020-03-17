7d644f89ea7394d973d179a79753040b37e22f41
Miles <milesasp@gmail.com>
2020-03-17 22:55:03 +0800

    add BasicAuth

--- main.go
+++ main.go
@@ -7,16 +7,24 @@ import (
 )
 
 func main() {
-	// todo: Authorization
 	// todo: example test
 	// todo: refactor
 	// todo: enable swagger https://github.com/swaggo/gin-swagger
 	var enableSSL = flag.Bool("enableSSL", false, "To enable SSL by adding -enableSSL flag")
 	var sslport = flag.String("sslport", "443", "The port of https. 443 is default value.")
 	var port = flag.String("port", "8080", "The port of http. 8080 is default value.")
+	var acct = flag.String("account", "", "The account for basic authorization usage.")
+	var pwd = flag.String("password", "", "The password for basic authorization usage.")
+
 	flag.Parse()
 
 	r := gin.Default()
+
+	if *acct != "" && *pwd != "" {
+		r.Use(gin.BasicAuth(gin.Accounts{
+			*acct:*pwd,
+		}))
+	}
 
 	r.POST("/rmi", RemoveImageApi)
 	r.POST("/pullimage", PullImageApi)
